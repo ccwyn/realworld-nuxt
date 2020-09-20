80b0a8e6088e596b01cdec0f7f7f363325d05101
ccwyn <ccwyn93@163.com>
2020-09-20 18:45:00 +0800

    feat:初始化项目

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    home page
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'',
+    props:[''],
+    data () {
+      return {
+
+      };
+    },
+
+    components: {},
+
+    computed: {},
+
+    beforeMount() {},
+
+    mounted() {},
+
+    methods: {},
+
+    watch: {}
+
+  }
+
+</script>
+<style lang='' scoped>
+
+</style>
