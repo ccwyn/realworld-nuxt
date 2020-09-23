1c11e2f97fe1e1b86a7a2647ac01181b2d021623
ccwyn <ccwyn93@163.com>
2020-09-23 21:49:53 +0800

    feat: 展示作者相关信息

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -3,98 +3,80 @@
     <div class="banner">
       <div class="container">
         <h1>How to build webapps that scale</h1>
-        <div class="article-meta">
-          <a href="">
-            <img src="http://i.imgur.com/Qr71crq.jpg">
-          </a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp;
-            Follow Eric Simons
-            <span class="counter">(10)</span>
-          </button>
-          &nbsp;&nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp;
-            Favorite Post
-            <span class="counter">(29)</span>
-          </button>
-        </div>
+        <article-meta :article="article"></article-meta>
       </div>
     </div>
     <div class="container page">
       <div class="row article-content">
-        <div class="col-md-12" v-html="article.body">
+        <div
+          class="col-md-12"
+          v-html="article.body"
+        >
         </div>
       </div>
       <hr>
       <div class="article-actions">
-        <div class="article-meta">
-          <a href="profile.html">
-            <img src="http://i.imgur.com/Qr71crq.jpg">
-          </a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp;
-            Follow Eric Simons
-            <span class="counter">(10)</span>
-          </button>
-          &nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp;
-            Favorite Post
-            <span class="counter">(29)</span>
-          </button>
-        </div>
+        <article-meta :article="article"></article-meta>
       </div>
       <div class="row">
         <div class="col-xs-12 col-md-8 offset-md-2">
           <form class="card comment-form">
             <div class="card-block">
-              <textarea class="form-control" placeholder="Write a comment..." rows="3"></textarea>
+              <textarea
+                class="form-control"
+                placeholder="Write a comment..."
+                rows="3"
+              ></textarea>
             </div>
             <div class="card-footer">
-              <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img">
+              <img
+                src="http://i.imgur.com/Qr71crq.jpg"
+                class="comment-author-img"
+              >
               <button class="btn btn-sm btn-primary">Post Comment</button>
             </div>
           </form>
           <div class="card">
             <div class="card-block">
-              <p
-                class="card-text"
-              >With supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
             </div>
             <div class="card-footer">
-              <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img">
+              <a
+                href=""
+                class="comment-author"
+              >
+                <img
+                  src="http://i.imgur.com/Qr71crq.jpg"
+                  class="comment-author-img"
+                >
               </a>
               &nbsp;
-              <a href="" class="comment-author">Jacob Schmidt</a>
+              <a
+                href=""
+                class="comment-author"
+              >Jacob Schmidt</a>
               <span class="date-posted">Dec 29th</span>
             </div>
           </div>
           <div class="card">
             <div class="card-block">
-              <p
-                class="card-text"
-              >With supporting text below as a natural lead-in to additional content.</p>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
             </div>
             <div class="card-footer">
-              <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img">
+              <a
+                href=""
+                class="comment-author"
+              >
+                <img
+                  src="http://i.imgur.com/Qr71crq.jpg"
+                  class="comment-author-img"
+                >
               </a>
               &nbsp;
-              <a href="" class="comment-author">Jacob Schmidt</a>
+              <a
+                href=""
+                class="comment-author"
+              >Jacob Schmidt</a>
               <span class="date-posted">Dec 29th</span>
               <span class="mod-options">
                 <i class="ion-edit"></i>
@@ -109,26 +91,29 @@
 </template>
 
 <script>
-import {getArticle} from "@/api/article";
-import MarkdownIt from 'markdown-it'
+import { getArticle } from "@/api/article";
+import MarkdownIt from "markdown-it";
+import ArticleMeta from "./components/article-mete";
 export default {
   name: "ArticleIndex",
   props: [""],
-  async asyncData({params}){
-    const {data} = await getArticle(params.slug)
-    const {article} =data
-    const md = new MarkdownIt()
+  async asyncData({ params }) {
+    const { data } = await getArticle(params.slug);
+    const { article } = data;
+    const md = new MarkdownIt();
     console.log(data);
-    article.body = md.render(article.body)
+    article.body = md.render(article.body);
     return {
-      article
-    }
+      article,
+    };
   },
   data() {
     return {};
   },
 
-  components: {},
+  components: {
+    ArticleMeta,
+  },
 
   computed: {},
 
